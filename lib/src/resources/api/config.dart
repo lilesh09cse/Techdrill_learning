fd069cb830fc12e5224dc51afd8ecbb8ffb7e2e2
Lilesh Gawande <30534262+lilesh09cse@users.noreply.github.com>
2021-05-17 09:01:28 +0530

    key

--- lib/src/resources/api/config.dart
+++ lib/src/resources/api/config.dart
@@ -8,6 +8,6 @@ class ApiConfig {
   ApiConfig() {
     baseUrl = "https://api.themoviedb.org/3";
     imageUrl = "https://image.tmdb.org/t/p/original/";
-    apiKey = "e162e6c1f09a49166561f9282429631f";
+    apiKey = "";
   }
 }
